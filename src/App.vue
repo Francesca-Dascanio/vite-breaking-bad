e6837c315abe2352f85549cff04940ea2acd5071
“Francesca <dascanio.francesca@gmail.com>
2023-02-06 14:29:34 +0100

    Inizializzazione progetto

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name: 'App',
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
